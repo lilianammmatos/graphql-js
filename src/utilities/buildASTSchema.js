154107e5816473a99cb45cb75957c9d9af22b44f
Liliana Matos <liliana@1stdibs.com>
2019-08-23 15:59:53 -0400

    implicitly add defer directive to schema

--- src/utilities/buildASTSchema.js
+++ src/utilities/buildASTSchema.js
@@ -15,6 +15,7 @@ import { GraphQLSchema } from '../type/schema';
 import {
   GraphQLSkipDirective,
   GraphQLIncludeDirective,
+  GraphQLDeferDirective,
   GraphQLDeprecatedDirective,
   GraphQLSpecifiedByDirective,
 } from '../type/directives';
@@ -102,19 +103,23 @@ export function buildASTSchema(
 
   const { directives } = config;
   // If specified directives were not explicitly declared, add them.
-  if (!directives.some((directive) => directive.name === 'skip')) {
+  if (!directives.some(directive => directive.name === 'skip')) {
     directives.push(GraphQLSkipDirective);
   }
 
-  if (!directives.some((directive) => directive.name === 'include')) {
+  if (!directives.some(directive => directive.name === 'include')) {
     directives.push(GraphQLIncludeDirective);
   }
 
-  if (!directives.some((directive) => directive.name === 'deprecated')) {
+  if (!directives.some(directive => directive.name === 'defer')) {
+    directives.push(GraphQLDeferDirective);
+  }
+
+  if (!directives.some(directive => directive.name === 'deprecated')) {
     directives.push(GraphQLDeprecatedDirective);
   }
 
-  if (!directives.some((directive) => directive.name === 'specifiedBy')) {
+  if (!directives.some(directive => directive.name === 'specifiedBy')) {
     directives.push(GraphQLSpecifiedByDirective);
   }
 
