523f2745fd1a370471165318aaab41bbbc02657e
Rob Richard <rob@1stdibs.com>
2020-01-08 17:21:44 -0500

    move dispatcher to execute, export ExecutionPatchResult

--- src/execution/index.js
+++ src/execution/index.js
@@ -3,6 +3,7 @@
 export { pathToArray as responsePathAsArray } from '../jsutils/Path';
 
 export { execute, defaultFieldResolver, defaultTypeResolver } from './execute';
+export type { ExecutionPatchResult } from './dispatcher';
 export type { ExecutionArgs, ExecutionResult } from './execute';
 
 export { getDirectiveValues } from './values';
