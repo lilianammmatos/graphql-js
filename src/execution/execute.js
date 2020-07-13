d0bac6357093d6728716a00a0f63f8ea8149c73a
Rob Richard <rob@1stdibs.com>
2020-07-13 17:22:49 -0400

    move dispatcher back to execute.js

--- src/execution/execute.js
+++ src/execution/execute.js
@@ -68,13 +68,6 @@ import {
 import { typeFromAST } from '../utilities/typeFromAST';
 import { getOperationRootType } from '../utilities/getOperationRootType';
 
-import type {
-  ExecutionResult,
-  ExecutionPatchResult,
-  AsyncExecutionResult,
-} from './dispatcher';
-import { Dispatcher } from './dispatcher';
-
 import {
   getVariableValues,
   getArgumentValues,
@@ -126,6 +119,42 @@ export type FormattedExecutionResult = {|
   extensions?: ObjMap<mixed>,
 |};
 
+/**
+ * The result of GraphQL execution.
+ *
+ *   - `errors` is included when any errors occurred as a non-empty array.
+ *   - `data` is the result of a successful execution of the query.
+ *   - `extensions` is reserved for adding non-standard properties.
+ *   - `hasNext` is true if a future payload is expected.
+ */
+export type ExecutionResult = {|
+  errors?: $ReadOnlyArray<GraphQLError>,
+  data?: ObjMap<mixed> | null,
+  extensions?: ObjMap<mixed>,
+  hasNext?: boolean,
+|};
+
+/**
+ * The result of an asynchronous GraphQL patch.
+ *
+ *   - `errors` is included when any errors occurred as a non-empty array.
+ *   - `data` is the result of the additional asynchronous data.
+ *   - `path` is the location of data.
+ *   - `label` is the label provided to @defer or @stream.
+ *   - `extensions` is reserved for adding non-standard properties.
+ *   - `hasNext` is true if a future payload is expected.
+ */
+export type ExecutionPatchResult = {|
+  errors?: $ReadOnlyArray<GraphQLError>,
+  data?: ObjMap<mixed> | mixed | null,
+  path?: $ReadOnlyArray<string | number>,
+  label?: string,
+  extensions?: ObjMap<mixed>,
+  hasNext: boolean,
+|};
+
+export type AsyncExecutionResult = ExecutionResult | ExecutionPatchResult;
+
 export type ExecutionArgs = {|
   schema: GraphQLSchema,
   document: DocumentNode,
@@ -142,8 +171,6 @@ export type FieldsAndPatches = {
   patches: Array<{| label?: string, fields: ObjMap<Array<FieldNode>> |}>,
   ...
 };
-
-export type { ExecutionResult, ExecutionPatchResult, AsyncExecutionResult };
 
 /**
  * Implements the "Evaluating requests" section of the GraphQL specification.
@@ -1164,25 +1191,10 @@ function completeAsyncIterableValue(
             index,
             path,
             result,
-            (patchValue, patchFieldPath, patchErrors) =>
-              completeValue(
-                exeContext,
-                itemType,
-                fieldNodes,
-                info,
-                patchFieldPath,
-                patchValue,
-                patchErrors,
-              ),
-            (error, patchFieldPath, patchErrors) =>
-              handleFieldError(
-                error,
-                fieldNodes,
-                patchFieldPath,
-                itemType,
-                exeContext,
-                patchErrors,
-              ),
+            exeContext,
+            fieldNodes,
+            info,
+            itemType,
           );
           return;
         }
@@ -1626,4 +1638,203 @@ export function getFieldDef(
     return TypeNameMetaFieldDef;
   }
   return parentType.getFields()[fieldName];
+}
+
+/**
+ * Same as ExecutionPatchResult, but without hasNext
+ */
+type DispatcherResult = {|
+  errors?: $ReadOnlyArray<GraphQLError>,
+  data?: ObjMap<mixed> | mixed | null,
+  path: $ReadOnlyArray<string | number>,
+  label?: string,
+  extensions?: ObjMap<mixed>,
+|};
+
+/**
+ * Dispatcher keeps track of subsequent payloads that need to be delivered
+ * to the client. After initial execution, returns an async iteratable of
+ * all the AsyncExecutionResults as they are resolved.
+ */
+export class Dispatcher {
+  _subsequentPayloads: Array<Promise<IteratorResult<DispatcherResult, void>>>;
+  _initialResult: ?ExecutionResult;
+  _hasReturnedInitialResult: boolean;
+
+  constructor() {
+    this._subsequentPayloads = [];
+    this._hasReturnedInitialResult = false;
+  }
+
+  hasSubsequentPayloads() {
+    return this._subsequentPayloads.length !== 0;
+  }
+
+  add(
+    label?: string,
+    path?: Path,
+    promiseOrData: PromiseOrValue<ObjMap<mixed> | mixed>,
+    errors: Array<GraphQLError>,
+  ): void {
+    this._subsequentPayloads.push(
+      Promise.resolve(promiseOrData).then((data) => ({
+        value: createPatchResult(data, label, path, errors),
+        done: false,
+      })),
+    );
+  }
+
+  addAsyncIterable(
+    label?: string,
+    nextIndex: number,
+    path?: Path,
+    result: AsyncIterable<mixed>,
+    exeContext: ExecutionContext,
+    fieldNodes: $ReadOnlyArray<FieldNode>,
+    info: GraphQLResolveInfo,
+    itemType: GraphQLOutputType,
+  ): void {
+    // $FlowFixMe
+    const iteratorMethod = result[SYMBOL_ASYNC_ITERATOR];
+    const iterator = iteratorMethod.call(result);
+    let index = nextIndex;
+    const handleNext = () => {
+      const fieldPath = addPath(path, index);
+      const patchErrors = [];
+      this._subsequentPayloads.push(
+        iterator.next().then(
+          ({ value: data, done }) => {
+            if (done && !data) {
+              return { value: undefined, done };
+            }
+            index++;
+            handleNext();
+            return {
+              value: createPatchResult(
+                completeValue(
+                  exeContext,
+                  itemType,
+                  fieldNodes,
+                  info,
+                  fieldPath,
+                  data,
+                  patchErrors,
+                ),
+                label,
+                fieldPath,
+                patchErrors,
+              ),
+              done,
+            };
+          },
+          (error) => {
+            handleFieldError(
+              error,
+              fieldNodes,
+              fieldPath,
+              itemType,
+              exeContext,
+              patchErrors,
+            );
+            return {
+              value: createPatchResult(null, label, fieldPath, patchErrors),
+              done: false,
+            };
+          },
+        ),
+      );
+    };
+
+    return handleNext();
+  }
+
+  _race(): Promise<IteratorResult<ExecutionPatchResult, void>> {
+    return new Promise((resolve) => {
+      this._subsequentPayloads.forEach((promise) => {
+        promise.then(() => {
+          // resolve with actual promise, not resolved value of promise
+          // so we can remove it from this._subsequentPayloads
+          resolve({ promise });
+        });
+      });
+    })
+      .then(({ promise }) => {
+        this._subsequentPayloads.splice(
+          this._subsequentPayloads.indexOf(promise),
+          1,
+        );
+        return promise;
+      })
+      .then(({ value, done }) => {
+        if (done && this._subsequentPayloads.length === 0) {
+          // async iterable resolver just finished and no more pending payloads
+          return {
+            value: {
+              hasNext: false,
+            },
+            done: false,
+          };
+        } else if (done) {
+          // async iterable resolver just finished but there are pending payloads
+          // return the next one
+          return this._race();
+        }
+        const returnValue: ExecutionPatchResult = {
+          ...value,
+          hasNext: this._subsequentPayloads.length > 0,
+        };
+        return {
+          value: returnValue,
+          done: false,
+        };
+      });
+  }
+
+  _next(): Promise<IteratorResult<AsyncExecutionResult, void>> {
+    if (!this._hasReturnedInitialResult) {
+      this._hasReturnedInitialResult = true;
+      return Promise.resolve({
+        value: {
+          ...this._initialResult,
+          hasNext: true,
+        },
+        done: false,
+      });
+    } else if (this._subsequentPayloads.length === 0) {
+      return Promise.resolve({ value: undefined, done: true });
+    }
+    return this._race();
+  }
+
+  get(initialResult: ExecutionResult): AsyncIterable<AsyncExecutionResult> {
+    this._initialResult = initialResult;
+    return ({
+      [SYMBOL_ASYNC_ITERATOR]() {
+        return this;
+      },
+      next: () => this._next(),
+    }: any);
+  }
+}
+
+function createPatchResult(
+  data: ObjMap<mixed> | mixed | null,
+  label?: string,
+  path?: Path,
+  errors?: $ReadOnlyArray<GraphQLError>,
+): DispatcherResult {
+  const value: DispatcherResult = {
+    data,
+    path: path ? pathToArray(path) : [],
+  };
+
+  if (label != null) {
+    value.label = label;
+  }
+
+  if (errors && errors.length > 0) {
+    value.errors = errors;
+  }
+
+  return value;
 }
